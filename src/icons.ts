00dcbe53576a31110e3c1c5e103f7f3338477bb0
Wang Ning <ning4.wang@intel.com>
2023-10-20 16:57:57 +0800

    add license header

--- src/icons.ts
+++ src/icons.ts
@@ -1,3 +1,9 @@
+/*-------------------------------------------------------------------------------------------------
+ *  Copyright (C) 2023 Intel Corporation. All rights reserved.
+ *  Licensed under the Apache License 2.0. See LICENSE in the project root for license information.
+ *  SPDX-License-Identifier: Apache-2.0
+ *-----------------------------------------------------------------------------------------------*/
+
 import * as H from "./helper";
 import fs from "graceful-fs";
 import path from "path";
